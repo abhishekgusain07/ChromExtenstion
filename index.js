8b1b1c102f6dd91ac7e897c6c0285e331a7c62d9
abhishek_gusain <abhishek.gusain1007fb@gmail.com>
2022-07-19 17:50:23 +0530

    extension added

---
+++ index.js
@@ -0,0 +1,28 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    // Clear out the input field
+    inputEl.value=" "
+    
+    //renders
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        //listItems += "<li> <a target = '_blank' href = ' " + myLeads[i] + "'>" +myLeads[i] + " </a></li>"
+        listItems += `
+            <li>
+                <a target='_blank' href = '${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li> 
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
