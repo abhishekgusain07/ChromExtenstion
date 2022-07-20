16102b11264d65aca7da9feef414f11ce60377c5
abhishek_gusain <abhishek.gusain1007fb@gmail.com>
2022-07-20 18:28:36 +0530

    loacalstorage feature added

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-let myLeads = []
+let myLeads = ["abhishek"]
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+
+//NEW
+// Get the leads from the localStorage - PS: JSON.parse()
+// store it in a variable, leadsFromLocalSotrage
+// Log out the variable
+let leadsfromlocalStorage = JSON.parse(localStorage.getItem("myleads"))
+
+//checking if leadfromstorage is empty or not, if yes rendering out previous stored leads
+if(leadsfromlocalStorage){
+    myLeads = leadsfromlocalStorage
+    renderLeads()
+}
+
+
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     // Clear out the input field
     inputEl.value=" "
     
+    //NEW
+    //saving myleads array to localstorage
+    //PS: remember JSON.stringigy()
+    localStorage.setItem("myleads", JSON.stringify(myLeads))
+    
     //renders
     renderLeads()
+    
+    //To verify that it works
+    console.log( localStorage.getItem("myleads"))
 })
 
 function renderLeads() {
